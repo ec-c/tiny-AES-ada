c23f8b007a34d36df4899ee9f389c7d52f80f5b8
dpr <dennis@przytarski.com>
2024-06-22 22:20:35 +0200

    added sbox and sub_bytes func.

--- aes.adb
+++ aes.adb
@@ -4,15 +4,29 @@ package body AES is
    --  Note any IV/nonce should never be reused with the same key.
    package body CTR is
 
-      function Xcrypt (This : Buffer; Buffer : T_Array) return T_Array is
+      function Xcrypt (This : in out Buffer; Buffer : T_Array) return T_Array is
       begin
          for Buf in Buffer'Range loop
-            null;
+            This.State := Sub_Bytes (@);
          end loop;
 
          return [];
       end Xcrypt;
 
    end CTR;
+
+   --  The Sub_Bytes procedure substitutes the values in the state matrix with
+   --  values in an S-box.
+   function Sub_Bytes (State : State_T) return State_T is
+      Result : State_T := State;
+   begin
+      for I in State'Range (1) loop
+         for J in State'Range (2) loop
+            Result (I, J) := SBox (T_Index (State (I, J)));
+         end loop;
+      end loop;
+
+      return Result;
+   end Sub_Bytes;
 
 end AES;
